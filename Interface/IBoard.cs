b7fd93835434158d433ee8bc73a7639a4232cbfe
SyahrulMub <syahrul.mubarrok4@gmail.com>
2023-07-29 14:20:15 +0700

    interface and game maker

---
+++ Interface/IBoard.cs
@@ -0,0 +1,6 @@
+namespace Dominoes;
+
+public interface IBoard
+{
+    bool SetBoard(int posX, int posY);
+}
---
+++ Interface/ITiles.cs
@@ -0,0 +1,6 @@
+namespace Dominoes;
+
+public interface ITiles
+{
+    void SetTilesValue(int sideA, int sideB);
+}
